c97eb086d5f9a37ef49ce070a1474cd5f76cfa31
Fajlur <fajlurgit@gmail.com>
2025-12-13 20:55:42 +0530

    Add req logging for IP debugging

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(deviceMiddleware);
 
+app.use((req, res, next)=>{
+    const forwardIP =req.headers['x-forwarded-for'];
+    const socketIP = req.socket.remoteAddress;
+    console.log(`[REQUEST] IP: ${forwardIP || socketIP} | URL: ${req.url}`);
+    next();
+})
+
 app.use("/", router);
 
 const PORT = process.env.PORT;
