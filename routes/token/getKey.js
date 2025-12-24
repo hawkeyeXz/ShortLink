1b3c077ea0fb64f881bdaad9fa814329bc411fc8
Fajlur <fajlurgit@gmail.com>
2025-12-24 19:15:48 +0530

    Added frontend

--- routes/token/getKey.js
+++ routes/token/getKey.js
@@ -4,7 +4,7 @@ import redis from "../../services/redisClient.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const deviceId = req.deviceId;
+  const deviceId = req.cookies.deviceId;
   const activeKey = await redis.get(`devkey:${deviceId}`);
   res.json({ token: activeKey || null });
 });
